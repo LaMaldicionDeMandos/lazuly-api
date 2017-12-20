3f526ccc250ee6d9f7875e22764270b8fe9c416d
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-12-19 22:08:43 -0300

    implemento registro de usuario

--- controllers/registry.js
+++ controllers/registry.js
@@ -1,12 +1,32 @@
 /**
  * Created by boot on 17/12/2017.
  */
+const request = require('request-promise');
+
+const HOST = config.get('host');
+const SIGNUP_URI = `${HOST}/auth/users/registration`;
+console.log(`Registration uri: ${SIGNUP_URI}`);
+
 class RegisterController {
 
   static register(req, res) {
     const body = req.body;
-    console.log(JSON.stringify(body));
-    res.status(201).send(body);
+    console.log(`request: ${JSON.stringify(body)}`);
+    const options = {
+      method: 'POST',
+      uri: SIGNUP_URI,
+      body: body,
+      json: true
+    };
+    request(options)
+      .then((result) => {
+        console.log(`Result: ${JSON.stringify(result)}`);
+        res.status(201).send()
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send(err);
+    });
   }
 }
 
