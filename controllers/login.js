c27665bbc2ec7df74f736075a595b2bd5dc05ca8
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-12-21 19:21:29 -0300

    trabajo en perdi mi contrasenña

--- controllers/login.js
+++ controllers/login.js
@@ -2,12 +2,20 @@
  * Created by boot on 17/12/2017.
  */
 const request = require('request-promise');
+const Redis = require("redis");
+const randtoken = require('rand-token');
+
+const redis = Redis.createClient(config.get('redis.port'), config.get('redis.host'));
 
 const HOST = config.get('host');
 const LOGIN_URI = `${HOST}/auth/oauth/token`;
 const API_KEY = config.get('auth.app_key');
 const SECRET = config.get('auth.secret');
 const BASIC = `Basic ${new Buffer(API_KEY + ":" + SECRET).toString("base64")}`;
+
+const EMAIL_URI = `${HOST}/mail`;
+const EMAIL_CODE = config.get('forgot.email_code');
+const APP_SECRET = config.get('secret');
 
 const getOptions = (form) => {
   return {
@@ -38,7 +46,7 @@ class LoginController {
         return res.status(201).send(result);
       })
       .catch((err) => {
-        console.log(`Error: ${err}`)
+        console.log(`Error: ${err}`);
         return res.status(400).send(err);
       });
   }
@@ -58,9 +66,36 @@ class LoginController {
         return res.status(201).send(result);
       })
       .catch((err) => {
-        console.log(`Error: ${err}`)
+        console.log(`Error: ${err}`);
         return res.status(400).send(err);
       });
+  }
+
+  static forgotPassword(req, res) {
+    const userEmail = req.params.user;
+    console.log(`${userEmail} olvido su contraseña`);
+    const token = randtoken.generate(16);
+    console.log(`generating token: ${token} for ${userEmail}`);
+    redis.set(userEmail, token, 'EX', 10); //expire in 10 seconds
+    redis.set(token, userEmail, 'EX', 10);
+
+    const email = {
+      code: EMAIL_CODE,
+      to: [userEmail],
+      payload: {token: token}
+    };
+    const options = {
+      method: 'POST',
+      headers: {
+        'X-Authorization-Secret': APP_SECRET
+      },
+      uri: EMAIL_URI,
+      body: email,
+      json: true
+    };
+    request(options)
+      .then(() => res.status(204).send())
+      .catch(() => res.status(500).send());
   }
 }
 
--- routes/login.js
+++ routes/login.js
@@ -5,5 +5,6 @@ const login = require('../controllers/login');
 /* GET home page. */
 router.post('/login', login.login);
 router.get('/refresh/:token', login.refresh);
+router.get('/forgot/:user', login.forgotPassword);
 
 module.exports = router;
