004aada0e899cfe92dc6c704ee94544139d8fa7c
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-12-21 12:26:21 -0300

    implemento endpoint para refresh_token

--- controllers/login.js
+++ controllers/login.js
@@ -8,25 +8,49 @@ const LOGIN_URI = `${HOST}/auth/oauth/token`;
 const API_KEY = config.get('auth.app_key');
 const SECRET = config.get('auth.secret');
 const BASIC = `Basic ${new Buffer(API_KEY + ":" + SECRET).toString("base64")}`;
+
+const getOptions = (form) => {
+  return {
+    method: 'POST',
+    uri: LOGIN_URI,
+    headers: {
+      'Authorization': BASIC,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form: form,
+    json: true
+  };
+};
 class LoginController {
 
   static login(req, res) {
     console.log(`Authorization: ${BASIC}`);
-    const options = {
-      method: 'POST',
-      uri: LOGIN_URI,
-      headers: {
-        'Authorization': BASIC,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      form: {
-        'grant_type': 'password',
-        'scope': 'web',
-        'username': req.body.username,
-        'password': req.body.password
-      },
-      json: true
-    };
+    const options = getOptions({
+      'grant_type': 'password',
+      'scope': 'web',
+      'username': req.body.username,
+      'password': req.body.password
+    });
+    request(options)
+      .then((result) => {
+        console.log(result);
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(201).send(result);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`)
+        return res.status(400).send(err);
+      });
+  }
+
+  static refresh(req, res) {
+    console.log(`Authorization: ${BASIC}`);
+    console.log(`Refresh Token: ${req.params.token}`);
+    const options = getOptions({
+      'grant_type': 'refresh_token',
+      'scope': 'web',
+      'refresh_token': req.params.token,
+    });
     request(options)
       .then((result) => {
         console.log(result);
--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const login = require('../controllers/login');
 
 /* GET home page. */
-router.post('/', login.login);
+router.post('/login', login.login);
+router.get('/refresh/:token', login.refresh);
 
 module.exports = router;
