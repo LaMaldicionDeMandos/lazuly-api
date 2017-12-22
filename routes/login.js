1d69ac81e8830a84d1af7a5a614115676df65afb
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-12-22 10:45:45 -0300

    trabajo en primera parte de olvide mi contrasenña

--- routes/login.js
+++ routes/login.js
@@ -5,6 +5,6 @@ const login = require('../controllers/login');
 /* GET home page. */
 router.post('/login', login.login);
 router.get('/refresh/:token', login.refresh);
-router.get('/forgot/:user', login.forgotPassword);
+router.post('/forgot/:user', login.forgotPassword);
 
 module.exports = router;
