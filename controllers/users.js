8df2151b530b68610b135dab886c99bdebfcb824
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-12-28 11:20:08 -0300

    implemento el get permissions

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,24 @@
 const request = require('request-promise');
 
 const HOST = config.get('host');
+const PERMISSIONS_URI = `${HOST}/auth/users`;
 const SECRET = config.get('secret');
 
 class UserController {
 
   static getPermissions(req, res) {
-    console.log('get Permissions user: ' + req.user.user_name);
-    res.send('ok ' + req.user.user_name);
+    const username = req.user.user_name;
+    console.log('get Permissions user: ' + username);
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-Authorization-Secret': SECRET
+      },
+      uri: `${PERMISSIONS_URI}/${username}/permissions`
+    };
+    request(options)
+      .then((permissions) => res.send(permissions))
+      .catch(() => res.status(500).send());
   }
 }
 
